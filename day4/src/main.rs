e81c059fa18a0a32a779f5f27f44ab6575ad6895
Artem Ogai <artem@ogai.dev>
2023-12-04 09:57:22 +0300

    day 4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -2,36 +2,67 @@ use utils::read_lines;
 
 #[derive(Debug)]
 struct Card {
-    pub id: u8,
+    pub _id: u8,
     pub winning_numbers: Vec<u8>,
     pub numbers: Vec<u8>,
 }
 
-fn main() {
-    let mut total_score = 0;
-    for line in read_lines("day4/input.txt").unwrap() {
-        let card = parse_card(&line.unwrap()).unwrap();
-        let score = calc_card_score(&card);
-        total_score += score;
+impl Card {
+    fn wins(&self) -> Vec<u8> {
+        let mut wins = Vec::new();
+        for num in &self.numbers {
+            if self.winning_numbers.contains(num) {
+                wins.push(*num);
+            }
+        }
+        wins
     }
-
-    println!("Total score: {}", total_score);
 }
 
-fn calc_card_score(card: &Card) -> u32 {
-    let mut score = 0;
-    let mut first = true;
-    for num in &card.numbers {
-        if card.winning_numbers.contains(num) {
-            if first {
-                score += 1;
-                first = false;
-            } else {
-                score *= 2;
+fn main() {
+    let mut cards = Vec::new();
+    for line in read_lines("day4/input.txt").unwrap() {
+        let card = parse_card(&line.unwrap()).unwrap();
+        cards.push(card);
+    }
+
+    let mut total_score = 0;
+    for card in &cards {
+        let score = calc_card_score(card);
+        total_score += score;
+    }
+    println!("Total score: {}", total_score);
+
+    let total_copies = calc_total_copies(&cards);
+    println!("Total copies: {}", total_copies);
+}
+
+fn calc_total_copies(cards: &[Card]) -> usize {
+    fn calc_copies(cards: &[Card], total: &mut usize) {
+        if let Some((head, tail)) = cards.split_first() {
+            *total += 1;
+            let copies = head.wins().len();
+            for i in 0..copies {
+                calc_copies(&tail[i..], total);
             }
         }
     }
-    score
+
+    let mut total = 0;
+    for i in 0..cards.len() {
+        calc_copies(&cards[i..], &mut total);
+    }
+    total
+}
+
+fn calc_card_score(card: &Card) -> u32 {
+    card.wins().split_first().map_or(0, |(_, tail)| {
+        let mut score = 1;
+        for _ in tail {
+            score *= 2;
+        }
+        score
+    })
 }
 
 fn parse_card(s: &str) -> Option<Card> {
@@ -44,7 +75,7 @@ fn parse_card(s: &str) -> Option<Card> {
         });
 
         id.zip(nums).map(|(id, (win_nums, nums))| Card {
-            id,
+            _id: id,
             winning_numbers: win_nums,
             numbers: nums,
         })
@@ -52,8 +83,7 @@ fn parse_card(s: &str) -> Option<Card> {
 }
 
 fn parse_numbers(s: &str) -> Option<Vec<u8>> {
-    s
-        .split_whitespace()
+    s.split_whitespace()
         .map(|num_str| num_str.parse::<u8>().ok())
         .collect()
 }
