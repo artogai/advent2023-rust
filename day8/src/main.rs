e1ab6367b783fbf689cee7e776d1641aa0b1996a
Artem Ogai <artem@ogai.dev>
2023-12-09 15:19:03 +0300

    day 8 part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -5,18 +5,19 @@ use utils::read_lines;
 fn main() {
     let lines = read_lines("day8/input.txt")
         .unwrap()
-        .into_iter()
         .collect::<Result<Vec<_>, std::io::Error>>()
         .unwrap();
-    let mut cmds = parse_cmd(&lines[0]);
+    let cmds = parse_cmd(&lines[0]);
     let map = parse_field(&lines[2..]);
 
+    // Part 1
+    let mut cmds1 = cmds.clone();
     let mut curr = "AAA";
     let end = "ZZZ";
 
     let mut steps = 0;
     while curr != end {
-        let cmd = cmds.next().unwrap();
+        let cmd = cmds1.next().unwrap();
         let (left, right) = map.get(curr).unwrap();
         match cmd {
             'L' => curr = left,
@@ -26,7 +27,54 @@ fn main() {
         steps += 1;
     }
 
-    println!("Steps: {}", steps);
+    println!("Steps 1: {}", steps);
+
+    // Part 2
+    let mut curr = map.keys().filter(|k| k.ends_with('A')).collect::<Vec<_>>();
+    curr.sort();
+
+    let oscilations = curr
+        .into_iter()
+        .map(|c| find_oscillation(c, cmds.clone(), &map))
+        .collect::<Vec<_>>();
+
+    println!("Steps 2: {}", lcm(&oscilations));
+}
+
+fn find_oscillation(
+    node: &str,
+    cmds: Commands,
+    field: &HashMap<&str, (&str, &str)>,
+) -> u64 {
+    let mut node = node;
+    for (steps, cmd) in cmds.enumerate() {
+        let (left, right) = field.get(node).unwrap();
+        if node.ends_with('Z') {
+            return steps as u64;
+        }
+        match cmd {
+            'L' => node = left,
+            'R' => node = right,
+            _ => panic!("Invalid command"),
+        }
+    }
+    panic!("No oscillation found");
+}
+
+fn gcd_2(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        a
+    } else {
+        gcd_2(b, a % b)
+    }
+}
+
+fn lcm_2(a: u64, b: u64) -> u64 {
+    a / gcd_2(a, b) * b
+}
+
+fn lcm(numbers: &[u64]) -> u64 {
+    numbers.iter().copied().reduce(lcm_2).unwrap()
 }
 
 fn parse_field(lines: &[String]) -> HashMap<&str, (&str, &str)> {
@@ -51,12 +99,13 @@ fn parse_cmd(s: &str) -> Commands {
     }
 }
 
+#[derive(Clone)]
 struct Commands {
     cmds: Vec<char>,
     index: usize,
 }
 
-impl<'a> Iterator for Commands {
+impl Iterator for Commands {
     type Item = char;
 
     fn next(&mut self) -> Option<Self::Item> {
